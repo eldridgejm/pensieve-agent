bd42cdc844c1c06ddc32967bb37c1fb7b033afa8
Justin Eldridge <eldridgejm@gmail.com>
2020-08-22 16:21:50 -0700

    Bumped version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pensieve_agent",
-    version="1.0.0",
+    version="2.0.0",
     py_modules=["pensieve_agent"],
     install_requires=[],
     entry_points={"console_scripts": ["_pensieve-agent = pensieve_agent:main"]},
